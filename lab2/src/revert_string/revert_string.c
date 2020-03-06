08533438cf2b3ecee09455159b95de95653e4854
shurpumpel <alekseyfdtv2016@gmail.com>
2020-03-06 10:14:42 +0000

    added revertString

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -3,5 +3,15 @@
 void RevertString(char *str)
 {
 	// your code here
+    int N=strlen(str);
+    char* a=malloc(sizeof(char)*N+1);
+    int i;
+    for (i=0;i<N;i++){
+        a[i]=str[N-i-1];
+    }
+    for(i=0;i<N;i++){
+        str[i]=a[i];
+    }
+
 }
 
