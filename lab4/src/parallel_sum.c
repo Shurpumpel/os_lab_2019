5f01b8269826427a5ba3707646b8999aca341faa
shurpumpel <alekseyfdtv2016@gmail.com>
2020-04-12 15:26:51 +0000

    zombie added

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include <pthread.h>
+
+#include <getopt.h>
 
 struct SumArgs {
   int *array;
@@ -22,23 +25,67 @@ void *ThreadSum(void *args) {
 }
 
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
-
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
   uint32_t seed = 0;
   pthread_t threads[threads_num];
 
-  /*
-   * TODO:
-   * your code here
-   * Generate array here
-   */
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"threds_num", required_argument, 0, 0},
+                                      {"seed", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "f", options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+      case 0:
+        switch (option_index) {
+          case 0:
+            threads_num = atoi(optarg);
+            // your code here
+            // error handling
+            if (threads_num <= 0) {
+            printf("threads_num is a positive number\n");
+            return 1;
+            }
+            break;
+          case 1:
+            seed = atoi(optarg);
+            // your code here
+            // error handling
+            if (seed <= 0) {
+            printf("seed is a positive number\n");
+            return 1;
+            }
+            break;
+          case 2:
+            array_size = atoi(optarg);
+            // your code here
+            // error handling
+            if (array_size <= 0) {
+            printf("array_size is a positive number\n");
+            return 1;
+            }
+            break;
+          
+          default:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
 
   int *array = malloc(sizeof(int) * array_size);
 
